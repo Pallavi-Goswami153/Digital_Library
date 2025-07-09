54c5e74fca9060dad3fa5c90b6eb4d9d913d8035
Khushi Sharma <ks9459879@gmail.com>
2025-07-09 22:57:34 +0530

    sidenavbar-demo

--- src/Screens/Course/Course.jsx
+++ src/Screens/Course/Course.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-export function Course() {
-    const {cls}=useParams();
-    console.log(cls)
-  return (
-    <>
-    <h1>{cls}</h1>
-    </>
-  )
-}
+// SidebarLayout.js
+import React from 'react';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 
- 
+export const Course = () => {
+  return (
+    <Container fluid>
+      <Row>
+        {/* Sidebar */}
+        <Col md={2} className="bg-dark text-white min-vh-100 p-3">
+          <h4 className="mb-4">My Sidebar</h4>
+          <Nav defaultActiveKey="/home" className="flex-column">
+            <Nav.Link href="/home" className="text-white">Semester 1</Nav.Link>
+            <Nav.Link href="/profile" className="text-white">Semester 2</Nav.Link>
+            <Nav.Link href="/settings" className="text-white">Semester 3</Nav.Link>
+            <Nav.Link href="/logout" className="text-white">Semester 4</Nav.Link>
+            <Nav.Link href="/logout" className="text-white">Semester 5</Nav.Link>
+            <Nav.Link href="/logout" className="text-white">Semester 6</Nav.Link>
+          </Nav>
+        </Col>
+
+        {/* Main Content */}
+        <Col md={10} className="p-4">
+          <h2>BCA</h2>
+          <p>This is the main area of your dashboard.</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
