34c7d30e033acf9a5293c89cd0d03df276c8cf62
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-09 21:39:12 +0530

    contact page updated

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,11 +1,15 @@
 import { Container, Row, Col, Card, Button } from "react-bootstrap";
 import College from "../../assets/DDDD.jpg";
-import { Navigate } from "react-router-dom";
-// const handleNavigate = () => {
-
-// }
+import { Navigate,useNavigate } from "react-router-dom";
+    
 
 export const Home = () => {
+const navigate=useNavigate();
+const handleNavigate = (props) => {
+        navigate(`/${props.variant}`)
+
+}
+
     // const items = [
     //     { key: "bca", label: "BCA", icon: "🎓" },
     //     { key: "bba", label: "BBA", icon: "🎓" },
@@ -48,66 +52,21 @@ export const Home = () => {
                         height: "100vh",
                         position: "relative",
                         zIndex: 2,
-                        color: "#f8b400",
+                        color: "white"
+                        // color: "#f8b400",
                     }}
                 >
                     <Container>
                         <h1 className="text-center fw-semibold">Welcome To </h1>
                         <h1 className="text-center fw-semibold">MAHARAJA AGRASEN INSTITUTE OF</h1>
                         <h1 className="text-center fw-semibold">MANAGEMENT & TECHNOLOGY</h1>
-                        <h1 className="text-center fw-bold text-warning">DIGITAL LIBRARY</h1>
+                        <h1 className="text-center fw-bold ">DIGITAL LIBRARY</h1>
                     </Container>
                 </Container>
             </div>
 
-
-            {/* <section className="bg-light py-5">
-        <Container>
-          <h2 className="text-center text-black mb-4">E-Content</h2>
-          <Row className="g-4 justify-content-center">
-            {items.map((item) => (
-              <Col
-                key={item.key}
-                xs={12}
-                md={6}
-                lg={4}
-                className="d-flex justify-content-center"
-              >
-                <Card
-                  className="text-center shadow-sm"
-                  style={{
-                    backgroundColor:"rgb(105, 180, 241)",
-                    cursor:"pointer" ,
-                    width: "100%",
-                    maxWidth: "300px",
-                    borderRadius: "12px",
-                    padding: "20px",
-                  }}
-                  onClick={handleNavigate}
-                >
-                  <div style={{ fontSize: "2rem", marginBottom: "10px" }}>
-                    {item.icon}
-                  </div>
-                  <Card.Body>
-                    <Card.Title style={{ fontSize: "1rem", fontWeight: "600" }}>
-                      {item.label}
-                    </Card.Title>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </section>
-      <Card className="m-10 p-5 shadow-lg " style={{backgroundColor:"rgb(140, 165, 185)"}} >
-        <h1>Purpose of DIGITAL LIBRARY</h1>
-        <p>The Digital Library at Maharaja Agrasen Institute of Management & Technology has been developed to provide easy access of the resources like E-book, Journal, Syllabus, Previous Years University Questions, Time Table and other academic resources to the student of the institute.</p>
-        <Button className="bg-light" style={{color:"black", height:"50px" ,width:"100px"}}>Details</Button>
-      </Card>*/}
-
-
             <Container >
-                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded " style={{ minHeight: "250px" }}>
+                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded cursor-pointer " style={{ cursor: "pointer", minHeight: "250px" }} onClick={handleNavigate} variant="bca">
                     <Col>
                         <Card className=" text-dark" style={{ backgroundColor: "#f5f5dc", height: "240px" }}>
                             <Card.Body className="d-flex flex-column justify-content-center align-items-center">
@@ -121,30 +80,30 @@ export const Home = () => {
                             <p>
                                 Explore essential textbooks, reference materials, and guides curated to support your BCA curriculum. From programming to data structures, every page brings you closer to mastery.
                                 <br />
-                                <i>📖 Read more. Know more. Build more.</i>
+                                <b><i>📖 Read more. Know more. Build more.</i></b>
                             </p>
                         </div>
                     </Col>
                 </Row>
 
-                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded " style={{ minHeight: "250px" }}>
+                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded cursor-pointer" style={{ cursor: "pointer", minHeight: "250px" }} onClick={handleNavigate} variant={"bba"}>
                     <Col>
                         <p>
                             <h5>Build your business foundation.</h5>
 
                             Access textbooks and guides covering business principles, economics, communication, and entrepreneurship to support your BBA studies.
                             <br />
-                            <i>📖 Understand business. Think smart. Grow strong. </i>                        </p>
+                            <b><i>📖 Understand business. Think smart. Grow strong. </i>    </b>                    </p>
                     </Col>
                     <Col>
-                       <Card className=" text-dark" style={{ backgroundColor: "	#f7e7ce	", height: "240px" }}>
+                        <Card className=" text-dark" style={{ backgroundColor: "	#f7e7ce	", height: "240px" }}>
                             <Card.Body className="d-flex flex-column justify-content-center align-items-center">
                                 <h1>BBA</h1>
                             </Card.Body>
                         </Card>
                     </Col>
                 </Row>
-                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded " style={{ minHeight: "250px" }}>
+                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded " style={{ cursor: "pointer", minHeight: "250px" }} onClick={handleNavigate} variant={"mca"}>
                     <Col>
                         <Card className=" text-dark" style={{ backgroundColor: "#faf0e6", height: "240px" }}>
                             <Card.Body className="d-flex flex-column justify-content-center align-items-center">
@@ -158,20 +117,20 @@ export const Home = () => {
 
                             Dive into books on advanced programming, AI, machine learning, software engineering, and more—curated to empower your MCA journey.
                             <br />
-                            <i>📖 Code deeper. Think bigger. Lead smarter.    </i>                    </p>
+                            <b><i>📖 Code deeper. Think bigger. Lead smarter.    </i></b>                    </p>
                     </Col>
                 </Row>
-                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded" style={{ minHeight: "240px" }}>
+                <Row sm={1} md={2} lg={2} className="m-5 p-5 shadow-lg border rounded cursor-pointer" style={{ cursor: "pointer", minHeight: "250px" }} onClick={handleNavigate} variant={"mba"}>
                     <Col>
                         <p>
                             <h5>Turn insights into impact.</h5>
 
                             Explore strategic management, marketing, finance, HR, and business analytics resources—carefully selected to fuel your MBA ambitions.
                             <br />
-                            <i>📖 Learn strategy. Master leadership. Drive change.  </i>                      </p>
+                            <b><i>📖 Learn strategy. Master leadership. Drive change.  </i>   </b>                   </p>
                     </Col>
                     <Col >
-                       <Card className="text-dark" style={{ backgroundColor: "#fbeccf", height: "240px" }}>
+                        <Card className="text-dark" style={{ backgroundColor: "#fbeccf", height: "240px" }}>
                             <Card.Body className="d-flex flex-column justify-content-center align-items-center">
                                 <h1>MBA</h1>
                             </Card.Body>
@@ -179,24 +138,24 @@ export const Home = () => {
                     </Col>
                 </Row>
             </Container>
-          <Container className="my-5">
-    <Card className="p-5 shadow-lg border-0" style={{ backgroundColor: "#fef8f2 " }}>
-        <h1 className="mb-3 text-primary">Purpose of DIGITAL LIBRARY</h1>
-        <p className="mb-4 text-dark" style={{ fontSize: "1.5rem" }}>
-            The Digital Library at Maharaja Agrasen Institute of Management & Technology has been developed to provide easy access to resources like E-books, Journals, Syllabus, Previous Years' University Questions, Time Table, and other academic materials for the students of the institute.
-        </p>
-        <Button
-            className="rounded-pill px-4 py-2 border-0"
-            style={{
-                backgroundColor: "#003366", // deep navy to match navbar
-                color: "white",
-                transition: "all 0.3s ease"
-            }}
-        >
-            Details
-        </Button>
-    </Card>
-</Container>
+            <Container className="my-5">
+                <Card className="p-5  border-0" style={{ backgroundColor: "#fef8f2 " }}>
+                    <h1 className="mb-3 text-primary">Purpose of DIGITAL LIBRARY</h1>
+                    <p className="mb-4 text-dark" style={{ fontSize: "1.5rem" }}>
+                        The Digital Library at Maharaja Agrasen Institute of Management & Technology has been developed to provide easy access to resources like E-books, Journals, Syllabus, Previous Years' University Questions, Time Table, and other academic materials for the students of the institute.
+                    </p>
+                    <Button
+                        className="rounded-pill px-4 py-2 border-0"
+                        style={{
+                            backgroundColor: "#003366", // deep navy to match navbar
+                            color: "white",
+                            transition: "all 0.3s ease"
+                        }}
+                    >
+                        Details
+                    </Button>
+                </Card>
+            </Container>
 
         </>
     );
