7b05989adf40a5b40b70418452f098945b344308
Pallavi Goswami <165028447+Pallavi-Goswami153@users.noreply.github.com>
2025-07-11 15:05:13 +0530

    seach bar updated

--- client/src/Screens/EbookViewer/EbookViewer.jsx
+++ client/src/Screens/EbookViewer/EbookViewer.jsx
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import { Row,Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { FiEye } from 'react-icons/fi';
+import { FiDownload } from 'react-icons/fi';
+import { Search } from '../../Components/SearchBar/SearchBar';
+
+// import { PDFViewer } from '../../Components/PDFviewer/Pdf';
+
+
 
 export const EbookViewer = () => {
   const { category } = useParams();
   const [ebooks, setEbooks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [notFound, setNotFound] = useState(false);
+
+  //values match krne ke liye
+   const [input,setInput]=useState("");
+      let inputhandler=(e)=>{
+          const lowercase=e.target.value.toLowerCase();
+          setInput(lowercase);
+      }
 
   useEffect(() => {
     setLoading(true);
@@ -59,24 +74,49 @@ export const EbookViewer = () => {
     );
   }
 
+
+   const filteredData = ebooks.filter((el) => {
+        //if no input the return the original
+        if (input === '') {
+            return el;
+        }
+        //return the item which contains the user input
+        else {
+            return el.title.toLowerCase().includes(input)  //title match krega
+        }
+    })
   return (
     <div className="container mt-4">
-      <h3 className="mb-4 text-center">{category.charAt(0).toUpperCase() + category.slice(1)} Ebooks</h3>
+      <Row className='m-3'>
+        <Col>
+      <h3 
+      // className=" text-center"
+      >{category.charAt(0).toUpperCase() + category.slice(1)} Ebooks</h3>
+        </Col>
+        <Col className="text-end">
+      <Search op={inputhandler} input={input}/>
+      
+        </Col>
+      </Row>
       <table className="table table-bordered table-striped">
         <thead>
           <tr>
             <th>Sr No.</th>
             <th>Title</th>
-            <th>Link</th>
+            <th>View</th>
+            <th>Download</th>
           </tr>
         </thead>
         <tbody>
-          {ebooks.map((ebook, index) => (
+          {filteredData.map((ebook, index) => (
             <tr key={index}>
               <td>{index + 1}</td>
               <td>{ebook.title}</td>
               <td>
-                <a href={ebook.link} target="_blank" rel="noopener noreferrer">View</a>
+                <a href={ebook.link} target="_blank" rel="noopener noreferrer"><FiEye size={20}  style={{ cursor: "pointer" }} /></a>
+              </td>
+              <td>
+                <a href={ebook.link} target="_blank" rel="noopener noreferrer"><FiDownload size={20} style={{ cursor: "pointer" }} /></a>
               </td>
             </tr>
           ))}
